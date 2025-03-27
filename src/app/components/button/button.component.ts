3818ac6040b74bbf763dc9aa692a55c622ff06d6
SajjadHadi <sajjadhadi77@gmail.com>
2025-03-27 08:22:07 +0100

    Config PrimeNG and Font

---
+++ src/app/components/button/button.component.ts
@@ -0,0 +1,14 @@
+import { Component } from '@angular/core';
+import { Button } from 'primeng/button';
+
+@Component({
+  selector: 'app-button',
+  imports: [
+    Button
+  ],
+  templateUrl: './button.component.html',
+  styleUrl: './button.component.css'
+})
+export class ButtonComponent {
+
+}

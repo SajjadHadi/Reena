7ee028dc2362b5a8787ebd5e826d869296f119f4
SajjadHadi <sajjadhadi77@gmail.com>
2025-03-28 18:33:17 +0100

    Add footer, and Update logo component

--- src/app/components/footer/footer.component.ts
+++ src/app/components/footer/footer.component.ts
@@ -1,11 +1,31 @@
 import { Component } from '@angular/core';
+import { LogoComponent } from '../logo/logo.component';
+import { TableModule } from 'primeng/table';
+import { NgForOf } from '@angular/common';
+import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-footer',
-  imports: [],
+  imports: [
+    LogoComponent,
+    TableModule,
+    NgForOf,
+    RouterLink
+  ],
   templateUrl: './footer.component.html',
-  styleUrl: './footer.component.css'
 })
 export class FooterComponent {
-
+  items = [
+    ['News',
+      'Investor Relations',
+      'Careers',
+    ], [
+      'Get Started',
+      'Learn',
+      'FAQ',
+    ], [
+      'Discord',
+      'Events',
+      'Blog',]
+  ]
 }

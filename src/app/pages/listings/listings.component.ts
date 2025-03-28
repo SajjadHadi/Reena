a03eac2215604f66972d7aff4a177c7e1d61baaf
SajjadHadi <sajjadhadi77@gmail.com>
2025-03-28 19:13:58 +0100

    Implement basic listings page

--- src/app/pages/listings/listings.component.ts
+++ src/app/pages/listings/listings.component.ts
@@ -1,11 +1,18 @@
 import { Component } from '@angular/core';
+import { Home } from '../../interfaces/home';
+import mockData from '../../mock-data';
+import { CardComponent } from '../../components/card/card.component';
+import { FooterComponent } from '../../components/footer/footer.component';
 
 @Component({
   selector: 'app-listings',
-  imports: [],
+  imports: [
+    CardComponent,
+    FooterComponent
+  ],
   templateUrl: './listings.component.html',
-  styleUrl: './listings.component.css'
 })
 export class ListingsComponent {
+  homes: Home[] = mockData as Home[];
 
 }
